959cd5a4da277fc8b53ce0e5f843575b2d498dab
Matej Mazúr <matejmazur@outlook.com>
2023-09-24 23:24:16 +0200

    Improve passphrase handling and user prompts in encryption program
    Updated the Symetric Key Encryption program to strengthen passphrase handling and enhance user prompts. The passphrase input phase now uses Rfc2898DeriveBytes for a more secure key derivation, replacing the previous basic conversion. This change improves passphrase security by utilizing salt and a higher iteration value. The user prompts and error handling during input have also been made more robust, resulting in a better user experience. An option to catch when the encrypted message file does not exist has been added.

--- SymetricKeyEncryption/Program.cs
+++ SymetricKeyEncryption/Program.cs
@@ -1,29 +1,54 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-Console.Write("Passphrase: ");
-string passphrase = Console.ReadLine();   
+Console.WriteLine();
+Console.WriteLine("********************************");
+Console.WriteLine("**** Symetric Key Encryption ****");
+Console.WriteLine("********************************");
+Console.WriteLine();
 
-while(true)
+Console.Write("Enter passphrase: ");
+string? passphrase = Console.ReadLine();
+var salt = new byte[16];
+RandomNumberGenerator.Fill(salt);
+using var pbkdf2 = new Rfc2898DeriveBytes(passphrase ?? "Passphrase", salt, 600000, HashAlgorithmName.SHA256);
+
+var key = pbkdf2.GetBytes(32);
+
+while (true) 
 {
-    Console.WriteLine("");   
-    Console.WriteLine("1: Safely store message");   
+    Console.WriteLine();
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1: Safely store message");
     Console.WriteLine("2: Read message");
     Console.WriteLine("0: Exit");
-    Console.Write("> "); 
+    Console.Write("> ");
+    string? userInput = Console.ReadLine();
+    
+    if (!int.TryParse(userInput, out int result)) 
+    {
+        Console.WriteLine("Invalid input, please enter a number!");
+        continue;
+    }
 
-    int userInput = Convert.ToInt32(Console.ReadLine());
-    switch (userInput)
+    switch (result)
     {
         case 1:
-            Console.Write("Type a message to encrypt: ");
-            string message = Console.ReadLine();
-            string encryptedMessage = Encrypt(message, passphrase);
+            Console.WriteLine();
+            Console.Write("Enter a message to encrypt: ");
+            string message = Console.ReadLine() ?? "";
+            string encryptedMessage = Encrypt(message, key);
             File.WriteAllText("message.txt", encryptedMessage);
+            Console.WriteLine("Message successfully encrypted and stored.");
             break;
         case 2:
+            if (!File.Exists("message.txt"))
+            {
+                Console.WriteLine("No encrypted message found. Please store a message first.");
+                break;
+            }
             string encrypted = File.ReadAllText("message.txt");
-            string decryptedMessage = Decrypt(encrypted, passphrase);
+            string decryptedMessage = Decrypt(encrypted, key);
             Console.WriteLine(decryptedMessage);
             break;
         case 0:
@@ -34,24 +59,22 @@ while(true)
     }
 }
 
-
-
-static string Encrypt(string text, string passphrase)
+static string Encrypt(string text, byte[] key)
 {
-    using AesGcm aesGcm = new AesGcm(Encoding.UTF8.GetBytes(passphrase));
+    using AesGcm aesGcm = new AesGcm(key);
     byte[] nonce = new byte[AesGcm.NonceByteSizes.MaxSize];
     byte[] cipherText = new byte[Encoding.UTF8.GetByteCount(text)];
     byte[] tag = new byte[AesGcm.TagByteSizes.MaxSize];
 
     aesGcm.Encrypt(nonce, 
                    Encoding.UTF8.GetBytes(text), 
-                   cipherText,
+                   cipherText, 
                    tag);
 
     return $"{Convert.ToBase64String(nonce)}.{Convert.ToBase64String(cipherText)}.{Convert.ToBase64String(tag)}";
 }
 
-static string Decrypt(string cipherTextTagNonce, string passphrase)
+static string Decrypt(string cipherTextTagNonce, byte[] key)
 {
     string[] parts = cipherTextTagNonce.Split('.');
     byte[] nonce = Convert.FromBase64String(parts[0]);
@@ -60,7 +83,7 @@ static string Decrypt(string cipherTextTagNonce, string passphrase)
 
     byte[] decryptedData = new byte[cipherText.Length];
 
-    using AesGcm aesGcm = new AesGcm(Encoding.UTF8.GetBytes(passphrase));
+    using AesGcm aesGcm = new AesGcm(key);
     aesGcm.Decrypt(nonce, 
                    cipherText, 
                    tag, 
